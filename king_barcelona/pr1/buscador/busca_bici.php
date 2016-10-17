c405e6e71671cd1a170dd43df68b0fd068418a07
mlopezgalan <lopezgalan.miguel@gmail.com>
2016-10-17 19:28:33 +0200

    Conexión con la base de datos

--- king_barcelona/pr1/buscador/busca_bici.php
+++ king_barcelona/pr1/buscador/busca_bici.php
@@ -1,3 +1,14 @@
 <?php
-	echo "hola <a href='../../indice.html'> Pulsa aquí para volver</a>;
- ?>
+	$mysqli = new mysqli("localhost", "root", "", "projecte_1");
+	if ($mysqli->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+	echo $mysqli->host_info . "\n";
+
+	/*$mysqli = new mysqli("127.0.0.1", "usuario", "contraseña", "basedatos", 3306);
+	if ($mysqli->connect_errno) {
+	    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}*/
+
+	echo $mysqli->host_info . "\n";
+		echo "hola <a href='../../indice.html'> Pulsa aquí para volver</a>";
