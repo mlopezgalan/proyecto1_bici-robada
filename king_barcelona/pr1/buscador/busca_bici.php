93a9b80818abe3fd08e0b364390053c52ba95e43
mlopezgalan <lopezgalan.miguel@gmail.com>
2016-10-17 20:43:57 +0200

    Se empieza a crear el formulario

--- king_barcelona/pr1/buscador/busca_bici.php
+++ king_barcelona/pr1/buscador/busca_bici.php
@@ -1,4 +1,34 @@
-<?php
+
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+<head>
+	<title>Encuentra mi bici!</title>
+	<link rel="stylesheet" type="text/css" href="../../kingbarcelona_files/style.css">
+</head>
+<body>
+
+	<form action="busca_bici.php">
+		<table class="login_box" border>
+			<tr>
+				<td>
+					Titulo:
+				</td>
+				<td>
+					<input type="textbox" name="title">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Fecha:
+				</td>
+				<td>
+					<input type="date" name="theft_date">
+				</td>
+			</tr>
+		</table>
+	</form>
+	<?php
 	$mysqli = new mysqli("localhost", "root", "", "projecte_1");
 	if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
@@ -13,4 +43,14 @@
         echo "id: "	.  $fila[0] . " titulo: " . $fila[1] . "<br/>";
     }
 
-		echo "hola <a href='../../indice.html'> Pulsa aquí para volver</a>";
+		//echo "hola <a href='../../indice.html'> Pulsa aquí para volver</a>";
+	?>
+
+</body>
+</html>
+
+
+
+
+
+
