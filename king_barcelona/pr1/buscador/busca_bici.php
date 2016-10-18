fb7b3af830352b8eb7d62ff4bacf52022d08a5e3
mlopezgalan <lopezgalan.miguel@gmail.com>
2016-10-18 19:10:43 +0200

    mejora consulta SELECT y otros campos en busca_bici.php

--- king_barcelona/pr1/buscador/busca_bici.php
+++ king_barcelona/pr1/buscador/busca_bici.php
@@ -1,11 +1,21 @@
-<meta charse="utf-8">
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>busca mi bici</title>
+	</head>
+	<body>
 <table border width="90%">
 <?php
+	extract($_POST)
+	
 	$mysqli = new mysqli("localhost", "root", "", "projecte_1");
 	if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 	}
-	$con 	=	"SELECT * FROM `anunci` ";
+	$con 	=	"SELECT `anu_id`,`anu_titol`,`anu_data_anunci`,`anu_data_robatori`,`anu_descripcio_robatori`,`anu_foto`,`anu_compensacio`
+		FROM `anunci` WHERE 1";
+	//	echo $con;die;
+	$result	=	mysqli_query($mysqli,$con);
 
 	while ($fila = mysqli_fetch_row($result)) {
        echo "<tr>";
@@ -17,23 +27,24 @@
      	  echo "<td>Fecha del robo</td>";
       	 echo "<td>$fila[3]</td>";
        echo "<tr>";
-     	  echo "<td style='max-width: 25px;''><img src='../../bicis/"	.	$fila[12]	. "' style='width: 100%;' />
+     	  echo "<td style='max-width: 25px;'><img src='../../bicis/"	.	$fila[5]	. "' style='width: 100%;' />
 			</td>";
-			echo "<td colspan='3'> $fila[5]</td>";
+			echo "<td colspan='3'> $fila[4]</td>";
 		echo "<tr>";
-			if(empty($fila[13])){
-				echo "<td colspan='4'><center></center>Sin recompensa</td>";
+			if(empty($fila[6])){
+				echo "<td colspan='4'><center>Sin recompensa</center></td>";
 			}
 			else{
-				echo "<td colspan='4'><center>$fila[13]</center></td>";
+				echo "<td colspan='4'><center>Recomensa: $fila[6]€</center></td>";
 			}
 		echo "</tr>";
     }
 
-		echo "hola <a href='../../indice.html'> Pulsa aquí para volver</a>";
+		//echo "hola <a href='../../indice.html'> Pulsa aquí para volver</a>";
    ?>
-</table>
-
+	</table>
+</body>
+</html>
 
 
 
