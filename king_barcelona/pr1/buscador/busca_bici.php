7cd905c309d46c5ff713c750722661ed4c949dcc
mlopezgalan <lopezgalan.miguel@gmail.com>
2016-10-19 18:00:50 +0200

    Creación de consulta con diferentes WHERE

--- king_barcelona/pr1/buscador/busca_bici.php
+++ king_barcelona/pr1/buscador/busca_bici.php
@@ -6,15 +6,49 @@
 	<body>
 <table border width="90%">
 <?php
-	extract($_POST)
-	
+	//Extraemos las variables que recibimos del formulario
+	extract($_POST);
+	//Generamos una variable que nos dirá si ya hay una condición en la consulta previamente, o no.
+	$condicion	=	0;
 	$mysqli = new mysqli("localhost", "root", "", "projecte_1");
 	if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 	}
 	$con 	=	"SELECT `anu_id`,`anu_titol`,`anu_data_anunci`,`anu_data_robatori`,`anu_descripcio_robatori`,`anu_foto`,`anu_compensacio`
-		FROM `anunci` WHERE 1";
-	//	echo $con;die;
+		FROM `anunci`
+		WHERE";
+	//vamos montando la consulta conforme las variables recibidas y controlando si hemos tenido una condicion previa o no.
+	if($title!="")
+	{
+		if($condicion>0){
+			$con.=" AND (`anu_titol` = '$title')";
+		}
+		else{
+			$condicion++;
+			$con.=	" `anu_titol` = '$title'";
+		}
+		
+	}
+	if($theft_date!=""){
+		if($condicion>0){
+			$con.=" AND (`anu_data_robatori` = '$theft_date')";
+		}
+		else{
+			$condicion++;
+			$con.=	"`anu_data_robatori` = '$theft_date'";
+		}
+		if($location!=""){
+		if($condicion>0){
+			$con.=" AND (`anu_ubicacio_robatori` = '$location')";
+		}
+		else{
+			$condicion++;
+			$con.=	"`anu_ubicacio_robatori` = '$location'";
+		}
+	}
+		//echo $condicion;
+		//echo $con;die;
+	
 	$result	=	mysqli_query($mysqli,$con);
 
 	while ($fila = mysqli_fetch_row($result)) {
